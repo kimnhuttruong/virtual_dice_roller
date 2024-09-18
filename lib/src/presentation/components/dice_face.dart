7014ba365dfa1d2009313356344619226cea338b
Cau Be Xu Dua <volehoai070902@gmail.com>
2024-09-18 15:44:12 +0700

    change to url image

--- lib/src/presentation/components/dice_face.dart
+++ lib/src/presentation/components/dice_face.dart
@@ -15,25 +15,26 @@ class CustomDiceFace extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String diceNumber = "";
+    String url = "";
+
     switch (number) {
       case 1:
-        diceNumber = "pb_1.png";
+        url += "https://scontent.fsgn19-1.fna.fbcdn.net/v/t1.15752-9/459182922_585503570469002_1568941098303412007_n.png?_nc_cat=100&ccb=1-7&_nc_sid=9f807c&_nc_eui2=AeHKE_VnDXmri0wBbwO_oPUqWleWL2cv1UhaV5YvZy_VSDmCTXAdKpSw2nEzaKakHyUw1PQsU_T-tMgKl-gCnDKx&_nc_ohc=5EehGm8_IAgQ7kNvgFicNyH&_nc_ht=scontent.fsgn19-1.fna&_nc_gid=A9JQzWWzY930VLqbnGbkGOf&oh=03_Q7cD1QE4HWfWuXHXJdzf9DXVDXeJAZLjQGURw0vN54OhgDQgwg&oe=67120ACF";
         break;
       case 2:
-        diceNumber = "pb_2.png";
+        url += "https://scontent.xx.fbcdn.net/v/t1.15752-9/459251379_489518600557795_143072884890981896_n.png?_nc_cat=110&ccb=1-7&_nc_sid=0024fc&_nc_eui2=AeFxXX8u4AYcIMM1xwC2QKmTSP_UxeFUvdBI_9TF4VS90NfUjW0ibeNDQ5FG8POm-9-_NVJnyZGF9fE4Ig-1iidY&_nc_ohc=emGBhYtcEeAQ7kNvgEWFV3-&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&_nc_gid=AFQPxYuQVtj-jbubhBF5Xb4&oh=03_Q7cD1QFcqfMuSEjSTrqGCBDhKWqcC8_JdNobBO1cLjra0eqJ3g&oe=6711F765";
         break;
       case 3:
-        diceNumber = "pb_3.png";
+        url += "https://scontent.xx.fbcdn.net/v/t1.15752-9/458773052_1211439429984303_5754649544639617371_n.png?_nc_cat=103&ccb=1-7&_nc_sid=0024fc&_nc_eui2=AeEkl3URBpVlG7VKe-MeOITE_IFeG_0xlR_8gV4b_TGVHy0C2mEyUqZmNSK_6ZLBNKg8fWNODv3JyHChlfh9BZfk&_nc_ohc=6GbjLtjLMgsQ7kNvgHBl3NO&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&_nc_gid=AFQPxYuQVtj-jbubhBF5Xb4&oh=03_Q7cD1QGwPJ5cqJXaq2kkmzAhc5RY7z5uiSxYteqeZbEYJmhSfQ&oe=67121862";
         break;
       case 4:
-        diceNumber = "pb_4.png";
+        url += "https://scontent.xx.fbcdn.net/v/t1.15752-9/458720770_1052503942950905_9154611876704126876_n.png?_nc_cat=101&ccb=1-7&_nc_sid=0024fc&_nc_eui2=AeFGcazOFi4Aj6elB_wnIU_Qf7aOMAnGZYl_to4wCcZliefCjNe7wmpbzCja7TQbCucPaE2nyqOJoDlFsKpPA5Ho&_nc_ohc=FzTHFjvUGkEQ7kNvgEWv9Cb&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&_nc_gid=AFQPxYuQVtj-jbubhBF5Xb4&oh=03_Q7cD1QFDfmVApO9Vd3JpS7dF_HwkOsiHjDVzJblw7EbkF1nAFQ&oe=6711FCC5";
         break;
       case 5:
-        diceNumber = "pb_5.png";
+        url += "https://scontent.xx.fbcdn.net/v/t1.15752-9/459236769_556535163604970_1113617706066173765_n.png?_nc_cat=101&ccb=1-7&_nc_sid=0024fc&_nc_eui2=AeF7-zwKyu6I2LxecG66bUqcjvTbq-HvOTSO9Nur4e85NJoqVNume5sk944gGLSutZ7jewjVp4BDRPq1fCD3uVAU&_nc_ohc=wQLhvUhRwIQQ7kNvgG4L4IB&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&_nc_gid=AFQPxYuQVtj-jbubhBF5Xb4&oh=03_Q7cD1QGnWZSDqpfy9HVnFuZZnbxgyg6_0HS7k4Nqw_cloU5SiQ&oe=67120D6B";
         break;
       case 6:
-        diceNumber = "pb_6.png";
+        url += "https://scontent.xx.fbcdn.net/v/t1.15752-9/458753606_507836735210198_5500733764522750099_n.png?_nc_cat=107&ccb=1-7&_nc_sid=0024fc&_nc_eui2=AeGC01PVoJpDtCKp2c7YdoQKVdFVZ7TaQtJV0VVntNpC0l4mStrFjtf6_QGZrd-hwDIwwEmVPzS_ZOZaU5LcfqB_&_nc_ohc=Zb89aW9cmaIQ7kNvgH2tcNF&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&_nc_gid=AFQPxYuQVtj-jbubhBF5Xb4&oh=03_Q7cD1QHYXLQEYI_3_U-oWvgNz9OhIKper9jvWsnHhODPrKziyg&oe=6711EEE4";
         break;
     }
     return transform != null
@@ -41,9 +42,9 @@ class CustomDiceFace extends StatelessWidget {
             alignment: alignment,
             transform: transform!,
             child: CubeCustom(
-                widthAndHeight: widthAndHeight, diceNumber: diceNumber),
+                widthAndHeight: widthAndHeight, url: url),
           )
-        : CubeCustom(widthAndHeight: widthAndHeight, diceNumber: diceNumber);
+        : CubeCustom(widthAndHeight: widthAndHeight, url: url);
   }
 }
 
@@ -51,19 +52,17 @@ class CubeCustom extends StatelessWidget {
   const CubeCustom({
     super.key,
     required this.widthAndHeight,
-    required this.diceNumber,
+    required this.url,
   });
 
   final double widthAndHeight;
-  final String diceNumber;
+  final String url;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      
       padding: EdgeInsets.all(8),
       decoration: BoxDecoration(
-
         borderRadius: BorderRadius.circular(15),
         border: Border.all(color: Colors.black, width: 1),
         boxShadow: [
@@ -98,7 +97,7 @@ class CubeCustom extends StatelessWidget {
             color: Colors.grey.shade100,
             image: DecorationImage(
                 fit: BoxFit.cover,
-                image: AssetImage("assets/images/$diceNumber"))),
+                image: NetworkImage(url))),
       ),
     );
   }
